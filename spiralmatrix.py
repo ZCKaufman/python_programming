3325ac4b8d73bd2255de98c29d02b036b07a823a
Zachary Kaufman <zckman7@gmail.com>
2019-10-24 20:21:54 -0400

    Cleanup in progress

--- spiralmatrix.py
+++ spiralmatrix.py
@@ -11,41 +11,31 @@ class MyMatrix:
 
         i = 0
         while(i <= listSize):
-            #print("!!!", i, "!!!")
-            #print(matrix)
             j = 0
             listNum = len(matrix) - 1
-#            print("BEGIN:", matrix)
             if(len(matrix) != 0):
-#                print("IF")
                 # First row
                 for k in range(len(matrix[0])):
                     finalList.append(matrix[0][k])
                 matrix.remove(matrix[0])
-#                print("FIRST", "Matrix:", matrix, "finalList:", finalList, "listNum:", listNum)
                 # Last column
-#                print(matrix)
                 if(len(matrix) != 0):
                     listSize = len(matrix[0]) - 1
                     j = 0
                     while(j < listNum):
-#                        print(matrix, j, listSize)
                         finalList.append(matrix[j][listSize])
                         matrix[j].remove(matrix[j][listSize])
                         j += 1
-#                print("SECOND", "Matrix:", matrix, "finalList:", finalList, "listNum:", listNum)
                 # Bottom row
                 if(len(matrix) != 0):
                     listNum = len(matrix) - 1
                     listSize = len(matrix[listNum]) - 1
                     j = listSize
                     while(j >= 0):
-#                        print(matrix, listNum, j)
                         finalList.append(matrix[listNum][j])
                         matrix[listNum].remove(matrix[listNum][j])
                         j -= 1
                     matrix.remove(matrix[listNum])
-#                print("THIRD", "Matrix:", matrix, "finalList:", finalList, "listNum:", listNum)
                 # First column
                 j = 0
                 i = listNum - 1
@@ -55,8 +45,6 @@ class MyMatrix:
                         finalList.append(matrix[i][0])
                         matrix[i].remove(matrix[i][0])
                         i -= 1
-                    #matrix.remove(matrix[listNum][0])
-#                print("FOURTH", "Matrix:", matrix, "finalList:", finalList, "listNum:", listNum)
                 print(matrix, finalList)
             else:
                 print("----------ELSE-----------\n",finalList,"\n|||||||||||||||||||||")
