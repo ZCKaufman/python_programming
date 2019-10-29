c1744e6a8c55516123d59d9081ce72708af18ce1
Zachary Kaufman <zckman7@gmail.com>
2019-10-29 19:07:41 -0400

    Checks top 100
    WordCount.py now checks for the top 100 used words, puts them in a list, and returns them.

--- WordCount.py
+++ WordCount.py
@@ -4,7 +4,7 @@ class WordCount:
     def countWords(self, fileName: str):
         f = open(fileName, "r")
         i = 0
-        #f = "Hello. How are you today? I hope you're doing well...\nI would love to swim in the good ol' sea.\nUnfortunately, I live in Indiana..."
+        top100 = []
         dictionary = {}
         fileOpen = True
         while(fileOpen):
@@ -21,16 +21,21 @@ class WordCount:
                         dictionary[word] = 1
             f.close()
             fileOpen = False
+        #print(dictionary["driving"])
         length = len(dictionary)
-        top100 = []
-        currentTop = 0
-        currentTopKey = ""
-        for key in dictionary:
-            #print(key)
-            if dictionary.get(key) > currentTop:
-                currentTop = dictionary.get(key)
-                currentTopKey = key
-        print("There are", length, "distinct words in the", fileName, "document. With the most used word being \"" + currentTopKey + "\" with", currentTop, "uses.")
+        i = 0
+        while(i < 100):
+            currentTop = 0
+            currentTopKey = ""
+            for key in dictionary:
+                if dictionary.get(key) > currentTop:
+                    currentTop = dictionary.get(key)
+                    currentTopKey = key
+            top100.append(currentTopKey)
+            dictionary.pop(currentTopKey)
+            i += 1
+        print(top100)
+        #print("There are", length, "distinct words in the", fileName, "document. With the most used word being \"" + currentTopKey + "\" with", currentTop, "uses.")
 
 obj = WordCount()
 obj.countWords("shakespeares-complete.txt")
